8a0a9326335705dc0e8ea80f2a97efb29a41078e
Thien Nguyen <t.nguyen@outlook.com>
2017-01-23 15:48:52 +0000

    updated UX issue with big search

--- views/assets/frontpage.js
+++ views/assets/frontpage.js
@@ -3,31 +3,37 @@ foods = {}
 $.getJSON("data/random_food_list.json", function(food_list) {
     foods = food_list;
     generateFoodTyper();
-    setInterval(generateFoodTyper, 10000);
+    setInterval(generateFoodTyper, 2000);
 });
 
 
 function generateFoodTyper() {
     var item = getRandomFood(foods);
-    type(item, "phatsearch");
+    // type(item, "phatsearch");
+    writeMagic("phatsearch", item)
 }
 
 function getRandomFood(food_list) {
-    return food_list.dishes[Math.floor(Math.random() * (food_list.dishes.length))];
+    var item = food_list.dishes[Math.floor(Math.random() * (food_list.dishes.length))]
+    // console.log(item)
+    return item;
 }
 
-function type(string, element) {
-    divid = document.getElementById(element);
-    (function writer(i) {
-        if (string.length <= i++) {
-            divid.value = string;
-            return;
+
+function writeMagic(div, type_this){
+    // console.log("doing wandom magic")
+    // console.log(div, type_this)
+    var demo_input = document.getElementById(div);
+    // console.log(demo_input)
+    var index = 0;
+
+    window.next_letter = function() {
+        if (index <= type_this.length) {
+            demo_input.placeholder = type_this.substr(0, index++);
+            setTimeout("next_letter()", 50);
         }
-        divid.value = string.substring(0, i);
-        if (divid.value[divid.value.length - 1] != " ") divid.focus();
-        var rand = Math.floor(Math.random() * (100)) + 140;
-        setTimeout(function() {
-            writer(i);
-        }, rand);
-    })(0)
+    }
+
+    // console.log(demo_input.placeholder)
+    next_letter();
 }
