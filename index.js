046fb6d32a4b141910cb429b337f5dd5e65a7c81
Thien Nguyen <t.nguyen@outlook.com>
2017-01-27 17:25:36 +0000

    initial commit for profile

--- index.js
+++ index.js
@@ -142,6 +142,24 @@ app.post('/register', function (req, res) {
     res.render('register', checks);
 })
 
+// profile page
+
+app.get('/profile', function (req, res) {
+    var review = {
+        pauline: "this is good lol"
+    }
+    var param = {
+        name: "John",
+        registered_date: "August 2014",
+        profile_photo: "http://1.bp.blogspot.com/-c9_0_EBSqCg/UG_uaVO3K-I/AAAAAAAAD18/zY53ome7ZRw/s200/John+Cena+profile+pic",
+        user_location: "London",
+        no_reviews: 17,
+        verified: true,
+        reviews: review
+    }
+    res.render('profile', param);
+})
+
 // search item
 
 app.get('/search', function (req, res) {
