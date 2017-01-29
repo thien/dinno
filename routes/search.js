a057f520d2141d2f77a3121df6a94f4df2a11108
Thien Nguyen <t.nguyen@outlook.com>
2017-01-29 03:05:07 +0000

    updated results to include card

--- routes/search.js
+++ routes/search.js
@@ -6,18 +6,24 @@ app.locals.basedir = "." + '/views';
 module.exports = function(){
 
 	app.get('/search', function (req, res) {
-	    var food_item = req.query.food;
-	    var param = {
-	        food: food_item
-	    }
 	    console.log("someone's searching for", param)
 
+	    // will need to deal with queries
 	    db.query('SELECT * FROM availableFood WHERE(availableFood.Food LIKE ?)',[food_item], function (error, results, fields) {
 	        // error will be an Error if one occurred during the query
 	        // results will contain the results of the query
 	        // fields will contain information about the returned results fields (if any)
 	        console.log(fields);
 	    });
+
+	    // will need to get search results
+
+
+
+	   	var food_item = req.query.food;
+	    var param = {
+	        food: food_item
+	    }
 
 	    res.render('searchitem', param);
 	})
