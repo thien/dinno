e40018cad7b0aa67e3c54b750dd580895f9c446b
SimeonC96 <simeon@localhost.localdomain>
2017-02-27 17:48:50 +0000

    updated search.js

--- routes/search.js
+++ routes/search.js
@@ -1,5 +1,6 @@
 var db = require('../functions/database');
 var bf = require('../functions/basefunctions');
+var mysql = require('mysql')
 var express = require('express');
 var app = express();
 app.locals.basedir = "." + '/views';
@@ -7,15 +8,23 @@ app.locals.basedir = "." + '/views';
 module.exports = function(){
 
 	app.get('/search', function (req, res) {
-		// console.log(req.query);
+		console.log(req.query);
+		var query = "SELECT * FROM availableFood WHERE(availableFood.Food LIKE ?)"
+		var foods = req.query.food.split(" ")
 
+		for(i = 1; i < foods.length ; i++){
+			query += " OR (availableFood.Food LIKE ?)"
+		}
+		query += ";"
+		query = mysql.format(query,foods)
+		console.log(query)
 	    // will need to deal with queries
-	    // db.query('SELECT * FROM availableFood WHERE(availableFood.Food LIKE ?)',[food_item], function (error, results, fields) {
-	    //     // error will be an Error if one occurred during the query
-	    //     // results will contain the results of the query
-	    //     // fields will contain information about the returned results fields (if any)
-	    //     console.log(fields);
-	    // });
+	    db.query('SELECT * FROM availableFood WHERE(availableFood.Food LIKE ?)',foods, function (error, results, fields) {
+	        // error will be an Error if one occurred during the query
+	        // results will contain the results of the query
+	        // fields will contain information about the returned results fields (if any)
+	        console.log(fields);
+	    });
 
 	    // will need to get search results
 	    // user id's
