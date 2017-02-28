68c5239022611ccae65ec129e98c046b504c647c
JJ97 <johnmjennings97@gmail.com>
2017-02-28 18:33:46 +0000

    disablesubmit button if password invalid

--- views/assets/register.js
+++ views/assets/register.js
@@ -6,10 +6,12 @@ function validatePassword() {
   
   if (pass.length >= 8 && hasLetter.test(pass) && hasNumber.test(pass)){
     $("#password").removeClass("invalid-input").addClass("valid-input");
+    $("#btn-submit").prop("disabled", false);
     return true;
   }
   else{
     $("#password").removeClass("valid-input").addClass("invalid-input");
+    $("#btn-submit").prop("disabled", true);
     return false;
   }
 }
@@ -19,12 +21,13 @@ function verifyPassword() {
   var p2 = $("#password-verify").val()
   if (validatePassword() && p1 === p2){
     $("#password-verify").removeClass("invalid-input").addClass("valid-input");
+    $("#btn-submit").prop("disabled", false);
   }
   else{
     $("#password-verify").removeClass("valid-input").addClass("invalid-input");
+    $("#btn-submit").prop("disabled", true);
   }
 }
-
 
 $(document).ready(function(){
   $("#password").keyup(validatePassword);
