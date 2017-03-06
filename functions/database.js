3d81919113197886374a3d7903defae7fea2689f
Thien Nguyen <t.nguyen@outlook.com>
2017-03-06 17:40:04 +0000

    initial commit for db load setup

--- functions/database.js
+++ functions/database.js
@@ -4,23 +4,12 @@ const fs = require('fs');
 //database key
 var db_keys = {};
 try {
-    var k = JSON.parse(fs.readFileSync('./config/database.json', 'utf8'));
-    console.log(k);
-    db_keys = {
-        host: k.host,
-        user: k.user,
-        password: k.password,
-        database: k.database
-    }
-    // db_keys = { //temporary keys to work with my db 
-    //     host: "localhost",
-    //     user: "root",
-    //     password: "meme1234",
-    //     database: "testDatabase"
-    // }
+    var db_keys = JSON.parse(fs.readFileSync('./config/database.json', 'utf8'));
+
     console.log("Secret Database Keys are found locally.");
 } catch (err) {
     // secret file isn't found.
+    console.log(err);
     console.log("Secret Database Keys aren't found.");
     console.log("Attempting to Allocate from Heroku Servers.");
     db_keys = {
@@ -28,8 +17,10 @@ try {
         user: process.env.JAWSDB_USER,
         password: process.env.JAWSDB_PASS,
         database: process.env.JAWSDB_DB
-    }
+    };
 }
+
+db_keys.multipleStatements = true;
 
 var db = database.createConnection(db_keys);
 db.connect(function(err) {
@@ -40,4 +31,35 @@ db.connect(function(err) {
     console.log('Connected to Database - ' + db.threadId);
 });
 
+// load the database file.
+
+db.query("DROP DATABASE " + db_keys.database, function (error, results, fields) {
+    console.log("cleared database");
+    db.query("CREATE DATABASE " + db_keys.database, function (error, results, fields) {
+        console.log("created new database");
+        db = database.createConnection(db_keys);
+        db.connect(function(err) {
+            if (err) {
+                console.error('error connecting: ' + err.stack);
+                return;
+            } 
+            console.log('Connected to Database - ' + db.threadId);
+
+            var base_db_code = fs.readFileSync('./database/dinno_database_v1.sql').toString();
+
+            db.query(base_db_code, function (error, results, fields) {
+                if (error) { 
+                    console.log("found error with loading the database file.");
+                    console.log(error); 
+                }
+                else { 
+                    console.log('Loaded core database file.'); 
+                }
+            });
+        });
+    });
+});
+
+
 module.exports = db;
+
