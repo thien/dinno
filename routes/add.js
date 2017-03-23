14bcebff9a5cf521ba113aade9a7e0d4e67c7848
Thien Nguyen <t.nguyen@outlook.com>
2017-03-23 11:58:50 +0000

    added error messages for add.js

--- routes/add.js
+++ routes/add.js
@@ -1,24 +1,30 @@
-module.exports = function(){
-	var express = require('express');
-	var app = express();
-	var login = require('../functions/login');
-	app.locals.basedir = "." + '/views';
+module.exports = function() {
+    var express = require('express');
+    var app = express();
+    var login = require('../functions/login');
+    app.locals.basedir = "." + '/views';
 
-	app.get('/add', function (req, res) {
-			login.checkLogin(req, res).then(function(result) {
-        res.render('new_fooditem');
-      }, function(err) {
-        res.render('frontpage');
-      });
-	})
+    app.get('/add', function(req, res) {
+        login.checkLogin(req, res).then(function(result) {
+            res.render('new_fooditem');
+        }, function(err) {
+            var error_message = {
+				msg:"You're not logged in."
+			};
+			res.render('error', error_message);
+        });
+    })
 
-	app.post('/add', function (req, res) {
-		login.checkLogin(req, res).then(function(result) {
-      res.render('new_fooditem');
-    }, function(err) {
-      res.render('frontpage');
-    });
-	})
+    app.post('/add', function(req, res) {
+        login.checkLogin(req, res).then(function(result) {
+            res.render('new_fooditem');
+        }, function(err) {
+            var error_message = {
+				msg:"You're not logged in."
+			};
+			res.render('error', error_message);
+        });
+    })
 
-	return app;
+    return app;
 }();
