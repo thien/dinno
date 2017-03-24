a85378c7e598cee24d4a2723be5c5bc4f9a947ad
JJ97 <johnmjennings97@gmail.com>
2017-03-24 20:30:46 +0000

    pulls dinnos from db

--- database/dinno_database_v1.sql
+++ database/dinno_database_v1.sql
@@ -71,6 +71,7 @@ CREATE TABLE IF NOT EXISTS `Meal` (
   `MealID` int(11) NOT NULL AUTO_INCREMENT,
   `LocationID` int(11) NOT NULL,
   `UserID` int(11) NOT NULL,
+  `RecipientID` int(11) ,
   `Name` varchar(60) NOT NULL,
   `DateServed` date NOT NULL,
   `Description` varchar(500) NOT NULL,
@@ -84,18 +85,18 @@ CREATE TABLE IF NOT EXISTS `Meal` (
 -- Test data for table `Meal`
 --
 
-INSERT INTO `Meal` (`MealID`, `LocationID`, `UserID`, `Name`, `DateServed`, `Description`, `Image`) VALUES
-(1, 2, 6, 'Birds', '2017-01-12', 'They keep me up at night. Free to a good home.', 'http://i.imgur.com/RIi7RlF.png'),
-(2, 1, 8, 'Apples', '2017-02-08', 'Fresh from my back garden', 'http://i.imgur.com/FgSGTwD.png'),
-(3, 2, 6, 'Bread', '2017-03-02', 'Baked it yesterday', 'http://i.imgur.com/0b4FxjX.png'),
-(4, 1, 9, 'Chicken Wings', '2017-03-03', 'Too spicy for my little baby mouth', 'http://i.imgur.com/BZO1dmk.png'),
-(5, 1, 6, 'Lime', '2017-03-03', 'Put in coconut and shake it all up', 'http://i.imgur.com/qGwqKyk.png'),
-(6, 2, 7, 'Burger', '2017-02-12', 'Get it quick or I might just eat it', 'http://i.imgur.com/TN27JVi.png'),
-(7, 2, 8, 'Pizza', '2017-01-26', 'P-I-Z-Z-A', 'http://i.imgur.com/W50LFXw.png'),
-(8, 1, 4, 'Raspberries', '2017-01-28', 'Stole them from a farmer when he wasn\'t looking', 'http://i.imgur.com/KtUoI1n.png'),
-(9, 2, 4, 'Burrito', '2017-01-29', 'La he lamido', 'http://i.imgur.com/wx1aQh8.png'),
-(10, 1, 9, 'Pineapple', '2017-02-02', 'Took the photo in a field because I\'m artistic', 'http://i.imgur.com/GseR7AL.jpg'),
-(11, 1, 6, 'Nan\'s Stew', '2017-03-02', 'You\'ll grow into a strong young man', 'http://i.imgur.com/9uVJuWZ.jpg');
+INSERT INTO `Meal` (`MealID`, `LocationID`, `UserID`, `RecipientID`, `Name`, `DateServed`, `Description`, `Image`) VALUES
+(1, 2, 6, 8, 'Birds', '2017-01-12', 'They keep me up at night. Free to a good home.', 'http://i.imgur.com/RIi7RlF.png'),
+(2, 1, 8, 2, 'Apples', '2017-02-08', 'Fresh from my back garden', 'http://i.imgur.com/FgSGTwD.png'),
+(3, 2, 6, 9,'Bread', '2017-03-02', 'Baked it yesterday', 'http://i.imgur.com/0b4FxjX.png'),
+(4, 1, 9, 6,'Chicken Wings', '2017-03-03', 'Too spicy for my little baby mouth', 'http://i.imgur.com/BZO1dmk.png'),
+(5, 1, 6, NULL,'Lime', '2017-03-03', 'Put in coconut and shake it all up', 'http://i.imgur.com/qGwqKyk.png'),
+(6, 2, 7, NULL,'Burger', '2017-02-12', 'Get it quick or I might just eat it', 'http://i.imgur.com/TN27JVi.png'),
+(7, 2, 8, NULL,'Pizza', '2017-01-26', 'P-I-Z-Z-A', 'http://i.imgur.com/W50LFXw.png'),
+(8, 1, 4, 5,'Raspberries', '2017-01-28', 'Stole them from a farmer when he wasn\'t looking', 'http://i.imgur.com/KtUoI1n.png'),
+(9, 2, 4, NULL,'Burrito', '2017-01-29', 'La he lamido', 'http://i.imgur.com/wx1aQh8.png'),
+(10, 1, 9, NULL,'Pineapple', '2017-02-02', 'Took the photo in a field because I\'m artistic', 'http://i.imgur.com/GseR7AL.jpg'),
+(11, 1, 6, NULL,'Nan\'s Stew', '2017-03-02', 'You\'ll grow into a strong young man', 'http://i.imgur.com/9uVJuWZ.jpg');
 -- --------------------------------------------------------
 
 --
