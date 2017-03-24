c6df053f199ad8e431fe180ddd7b4102c073b0ec
Thien Nguyen <t.nguyen@outlook.com>
2017-03-24 19:29:00 +0000

    initial commit for manage dinnos

--- routes/manage.js
+++ routes/manage.js
@@ -7,6 +7,24 @@ var bf = require('../functions/basefunctions');
 
 app.locals.basedir = "." + '/views';
 
+
+/*
+
+Note: For /manage
+
+Feel free to argue against; since im not sure how best this should be implemented
+but in /manage you'd be able to see dinnos that you've uploaded and dinnos that you
+requested. This should work similarly to how the web FAQ summative works, so the JSON
+of the user's data (dinnos the user uploaded and the dinnos users requested) would
+be stored on client side; which means that data manipulation would also be client side
+through javascript.
+
+This is obviously a template; implementation is absent
+
+- Thien
+
+*/
+
 module.exports = function() {
 
 	app.get('/manage', function(req, res) {
@@ -18,7 +36,7 @@ module.exports = function() {
 			var cookies = new Cookies(req, res);
 			userId = cookies.get('id');
 
-			// var profileInfow = getProfileInfo(userId);
+			// var profileInfo = getProfileInfo(userId);
 
 			param.user_data = {
 				userID: userId,
@@ -27,18 +45,9 @@ module.exports = function() {
 				mugshot: result.ProfileImage
 			};
 
-			param.alerts = {
-					warning: [],
-					info : [],
-					error : [],
-					success : ["Your data has been updated successfully."]
-				}
 
-		
-
-				res.render('manage', param);
-
-			}, function(err) {
+			res.render('manage', param);
+		}, function(err) {
 			param.error_message = {
 				msg: "You're not logged in."
 			};
