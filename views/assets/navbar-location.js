d38720010591905420ad9851e4fbe57d82bd4721
Thien Nguyen <t.nguyen@outlook.com>
2017-03-25 15:02:59 +0000

    updated arrow colours

--- views/assets/navbar-location.js
+++ views/assets/navbar-location.js
@@ -6,16 +6,22 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
 
+var arrow_loading_interval = setInterval(function(){
+    $(".arrowloading").toggleClass("arrowpulse");
+ },500)
+
 
 $(document).on('click', '#locationarrow', function(){
 
     console.log("You clicked the arrow")
     // commence getting the coordinates
+    var arrow = document.getElementById("locationarrow");
+    arrow.classList.add("arrowloading");
+    arrow.classList.remove("arrowdone");
 
     // glow arrow to show that you are getting it.
     navigator.geolocation.getCurrentPosition(coord_base_success, coord_base_error);
 });
-
 
 function coord_base_success(position) {
     var latitude  = position.coords.latitude.toFixed(4).toString();
@@ -23,7 +29,11 @@ function coord_base_success(position) {
 
     var coords = latitude +","+ longitude
     console.log("gps coord:",coords);
-    document.getElementById("location_text").innerHTML = coords.toString()
+    // document.getElementById("location_text").innerHTML = coords.toString()
+
+    var arrow = document.getElementById("locationarrow");
+    arrow.classList.remove("arrowloading");
+    arrow.classList.add("arrowdone");
 
     storeCoordinatesinCookies(latitude,longitude);
     $('input[name="lat"]').val(latitude);
@@ -40,6 +50,9 @@ function coord_base_error() {
         // console.log(res)
         var latitude = res[0];
         var longitude = res[1];
+
+        arrow.classList.remove("arrowloading");
+        arrow.classList.add("arrowamber");
         
         storeCoordinatesinCookies(latitude,longitude);
         $('input[name="lat"]').val(latitude);
