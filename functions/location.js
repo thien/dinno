dc42a51d133b4f02fcdd068ca4a2774b0b81908f
JJ97 <johnmjennings97@gmail.com>
2017-04-04 15:27:10 +0100

    can add food from current location

---
+++ functions/location.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var Cookies = require("cookies");
+var dateFormat = require('dateformat');
+var request = require('request');
+const db = require('../functions/database');
+const MAPS_KEY = 'AIzaSyAnTlmvCsv7mHwE7yiFmHBEA7fzNENHq70';
+
+function getLocationInformation(lat, lng) {
+  return new Promise(function(resolve, reject) {
+      var url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${MAPS_KEY}` ;
+      request(url, function (error, resp, body) {
+        if (error) {
+          console.log(error);
+          reject(error);
+        }
+        else {
+          var res = JSON.parse(body).results[0].address_components.reduce(function(acc , x) {
+            acc[x.types[0]] = x.long_name;
+            return acc;
+          }, {});
+          resolve(res);
+        }
+      });
+  });
+}
+
+function checkLocationExists(lat, lng) {
+  return new Promise(function(resolve, reject) {
+    db.query(`SELECT LocationID
+              FROM Location
+              WHERE Latitude BETWEEN ? AND ?
+              AND Longitude BETWEEN ? AND ?`, 
+            [lat - 0.001, lat + 0.001, lng - 0.001, lng + 0.001],
+    function(error, results, fields) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } 
+      else if (results.length == 0) {
+        resolve(0);
+      }
+      else {
+        console.log(results);
+        resolve(results[0].LocationID);
+      }
+    });
+  });
+}
+
+module.exports = {
+  getLocationInformation: function(lat, lng) {
+    return getLocationInformation(lat, lng);
+  },
+
+  addNewLocation: function(lat,lng) {
+    return new Promise(function(resolve, reject) {
+        checkLocationExists(lat, lng).then( function(locationId) {
+          console.log(locationId);
+          if (locationId == 0) {
+            getLocationInformation(lat,lng).then( function(locData){
+              db.query(`INSERT INTO Location
+                        VALUES (0, ?, ?, ?, ?, ?, 0, ?, ?)`, 
+                      [locData.postal_code, locData.street_number || locData.premise , locData.route, locData.postal_town, locData.administrative_area_level_2, lat, lng],
+              function(error, results, fields) {
+                if (error) {
+                  console.log(error);
+                  reject(error);
+                } else {
+                  console.log(`Added location ${locData.route}`);
+                  resolve(results.insertId);
+                }
+              });
+            }, function(err) {
+              reject(err);
+            });
+          }
+          else {
+            resolve(locationId);
+          }
+        }, function(err) {
+          reject(err);
+        });
+    });
+  },
+
+}
--- routes/add.js
+++ routes/add.js
@@ -2,29 +2,35 @@ const express = require('express');
 const app = express();
 const db = require('../functions/database');
 var login = require('../functions/login');
+var location = require('../functions/location');
 var Cookies = require("cookies");
 
-function addNewMeal(mealData, userId) {
+function addNewMeal(mealData, userId, lat, lng) {
+	console.log(mealData);
 	return new Promise(function(resolve, reject) {
 		var year = mealData['year'];
 		var month = mealData['month'];
 		var day = mealData['day'];
 		var bestBefore = `${year}/${month}/${day}`;
-	
 		var isIngredient = mealData.foodtype == 'Ingredient';
 
-		db.query(`INSERT INTO Meal (MealID, LocationID, UserID, RecipientID, Name, BestBefore, Description, Image, IsIngredient)
-							VALUES (0, ?, ?, NULL, ?, ?, ?, ?, ?)`, 
-						[mealData.location, userId, mealData.name, bestBefore, mealData.description, mealData['images[]'], isIngredient],
-		function(error, results, fields) {
-			if (error) {
-				console.log(error);
-				reject(error);
-			} else {
-				console.log(`Added meal ${mealData.name}`);
-				resolve(results);
-			}
-		});
+		location.addNewLocation(lat,lng).then(function(locationId) { 
+			db.query(`INSERT INTO Meal (MealID, LocationID, UserID, RecipientID, Name, BestBefore, Description, Image, IsIngredient)
+								VALUES (0, ?, ?, NULL, ?, ?, ?, ?, ?)`, 
+							[locationId, userId, mealData.name, bestBefore, mealData.description, mealData['images[]'], isIngredient],
+			function(error, results, fields) {
+				if (error) {
+					console.log(error);
+					reject(error);
+				} else {
+					console.log(`Added meal ${mealData.name}`);
+					resolve(results);
+				}
+			});
+
+		}, function(err) {
+			reject(err);
+		});		
 	});
 }
 
@@ -72,7 +78,12 @@ module.exports = function() {
 			};
 
 			var mealData = req.body;
-			addNewMeal(mealData, result.UserID).then(function(result) {	
+
+			var cookies = new Cookies(req, res);
+		  var lat = cookies.get('lat');
+			var lng = cookies.get('lng');
+
+			addNewMeal(mealData, result.UserID, lat, lng).then(function(result) {	
 
 					param.new_item = {
 						name: mealData.name,
