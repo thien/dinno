98339de26b58d80b69e16b8673bcc64edd5d611c
Simeon Chan <simeon@localhost.localdomain>
2017-04-04 16:39:32 +0100

    added same item, in different place to test radius

--- database/dinno_database_v1.sql
+++ database/dinno_database_v1.sql
@@ -77,7 +77,8 @@ INSERT INTO `Meal` (`MealID`, `LocationID`, `UserID`, `RecipientID`, `Name`, `Be
 (8, 1, 4, 5,'Raspberries', '2017-01-28', 'Stole them from a farmer when he wasn\'t looking', 'http://i.imgur.com/KtUoI1n.png',1),
 (9, 2, 4, NULL,'Burrito', '2017-01-29', 'La he lamido', 'http://i.imgur.com/wx1aQh8.png',0),
 (10, 1, 9, NULL,'Pineapple', '2017-02-02', 'Took the photo in a field because I\'m artistic', 'http://i.imgur.com/GseR7AL.jpg',0),
-(11, 1, 6, NULL,'Nan\'s Stew', '2017-03-02', 'You\'ll grow into a strong young man', 'http://i.imgur.com/9uVJuWZ.jpg',0);
+(11, 1, 6, NULL,'Nan\'s Stew', '2017-03-02', 'You\'ll grow into a strong young man', 'http://i.imgur.com/9uVJuWZ.jpg',0),
+(12, 2, 8, 2, 'Apples', '2017-02-08', 'Found from some dudes back garden', 'http://i.imgur.com/FgSGTwD.png',1);
 -- --------------------------------------------------------
 
 --
