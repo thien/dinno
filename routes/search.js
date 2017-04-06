8b4a3e8c95a59893341e7f5dd1bc7724a86dd976
JJ97 <johnmjennings97@gmail.com>
2017-04-06 23:24:37 +0100

    can now search if logged out

--- routes/search.js
+++ routes/search.js
@@ -35,44 +35,10 @@ module.exports = function() {
                 surname: result.Surname,
                 mugshot: result.ProfileImage
             };
-
-            param.foodcheck = true;
-
-            req.query.food = req.query.food || "";
-            req.query.isMeal = req.query.isMeal || "both";
-
-            if (req.query.location && req.query.location !== "Locating Position...") {
-                // get coords from search parameter
-
-               geocoder.geocode(req.query.location, function(err, geo) {
-                  param.location = req.query.location;
-                  console.log(geo);
-                  console.log("allocating coords");
-                  req.query.lat = geo[0].latitude;
-	                req.query.lng = geo[0].longitude;
-	                console.log("got,", req.query.lat, "-", req.query.lng)
-	                param.lat = req.query.lat;
-	                param.lng = req.query.lng;
-	                console.log("allocated coordinates.");
-                  dealWithResults(req, res, param);
-              });
-
-            } else {
-              // dude didn't type in a thang
-              // default to durham when no position given.
-              req.query.lat = 54.7731;
-              req.query.lng = -1.57489;
-              param.lat = req.query.lat;
-              param.lng = req.query.lng;
-              dealWithResults(req, res, param);
-            }
-
+						processQuery(req, res, param);
 
         }, function(err) {
-            param.error_message = {
-                msg: "You're not logged in."
-            };
-            res.render('error', param);
+            processQuery(req, res, param);
         });
 
     })
@@ -96,6 +62,39 @@ function iterateDistance(req, results, i) {
     } else {
         console.log(data)
     }
+}
+
+function processQuery(req, res, param) {
+	param.foodcheck = true;
+
+  req.query.food = req.query.food || "";
+  req.query.isMeal = req.query.isMeal || "both";
+
+  if (req.query.location && req.query.location !== "Locating Position...") {
+      // get coords from search parameter
+
+     geocoder.geocode(req.query.location, function(err, geo) {
+        param.location = req.query.location;
+        console.log(geo);
+        console.log("allocating coords");
+        req.query.lat = geo[0].latitude;
+        req.query.lng = geo[0].longitude;
+        console.log("got,", req.query.lat, "-", req.query.lng)
+        param.lat = req.query.lat;
+        param.lng = req.query.lng;
+        console.log("allocated coordinates.");
+        dealWithResults(req, res, param);
+    });
+
+  } else {
+    // dude didn't type in a thang
+    // default to durham when no position given.
+    req.query.lat = 54.7731;
+    req.query.lng = -1.57489;
+    param.lat = req.query.lat;
+    param.lng = req.query.lng;
+    dealWithResults(req, res, param);
+  }
 }
 
 function dealWithResults(req, res, param) {
