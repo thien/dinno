7a136d23044ee91d92f376e19809e201cdc30173
rs96 <robert.shipley@durham.ac.uk>
2017-04-06 14:47:11 +0100

    sorting more

--- views/assets/interactive_sorting.js
+++ views/assets/interactive_sorting.js
@@ -1,27 +1,17 @@
 
-
-/*
-sorting methods:
- - best before
- - category(s)
- - distance
-*/
-
-//get some results from the database as a list of objects
-//then as the constraints are clicked, produce lists of the things
-
-//event trigger, when some constraint clicked
+// sorted results variable
+var sortedResults = fooditems.slice();
 
 //rewrite the card holder div
-function writeCardHolder(){
+function writeCardHolder(sortedResults){
 	var newContent = '';
-	for(var i=0; i<fooditems.length; i++){
+	for(var i=0; i<sortedResults.length; i++){
 		newContent += '<div class="col-md-6 col-lg-4"><a class="card" href="/fooditem" id="fooditem_'+fooditems[i].food_id+'">'+
 		'<img class="card-img-top img-fluid" src="https://unsplash.it/600,751/?random" alt="Card image cap">'+
         '<div class="card-profile-container"><img class="card-userprofile-img" src="https://unsplash.it/80,80/?random" alt="Card image cap"></div>'+
         '<div class="card-block">'+
-        '<h4 class="card-title">'+fooditems[i].foodname+'</h4>'+
-        '<p class="card-text">'+fooditems[i].description+'</p>'+
+        '<h4 class="card-title">'+sortedResults[i].foodname+'</h4>'+
+        '<p class="card-text">'+sortedResults[i].description+'</p>'+
         '<p class="card-text"><small class="text-muted">Last updated 6 mins ago</small></p>'+
         '</div>'+
         '</a></div>'
@@ -30,21 +20,36 @@ function writeCardHolder(){
 }
 
 
-//restrict results by distance
+// restrict results by distance
 function distanceRestriction(results, maxDistance){
 	//do stuff
 	maxDistance = maxDistance/111;	//do kilometers to degrees (positioning done in lat/log)
 	var userLat = 54.78, userLng = -1.52;
 	resultsInRadius = [];
+	console.log(maxDistance)
 	for(var i=0;i<results.length;i++){
-		if(Math.sqrt(Math.pow(results[i].marker.lat-userLat,2) + Math.pow(results[i].marker.lng-userLng,2))){
-			resultsInRadius.add(results[i]);
+		console.log(Math.sqrt(Math.pow(results[i].marker.lat-userLat,2) + Math.pow(results[i].marker.lng-userLng,2)));
+		if(Math.sqrt(Math.pow(results[i].marker.lat-userLat,2) + Math.pow(results[i].marker.lng-userLng,2))<maxDistance){
+			resultsInRadius.push(results[i]);
 		}
 	}
 	return resultsInRadius;
 }
 
-//restrict results by distance
+// restrict results by best before
+function bestBeforeRestriction(results, day, month, year){
+	resultsInDate = [];
+	var chosenDate = (new Date(year, month, day, 0, 0, 0, 0)).getTime();
+	for(var i=0;i<results.length;i++){
+		if(new Date(results[i].best_before).getTime()>chosenDate){
+			console.log(results[i].best_before);
+			resultsInDate.push(results[i]);
+		}
+	}
+	return resultsInDate;
+}
+
+//order results by distance
 function byNearest(results){
 	var userLat = 54.78, userLng = -1.52;
 	results.sort(function(a,b){
@@ -53,18 +58,7 @@ function byNearest(results){
 	});
 }
 
-//constraint is category
-function byCategory(results, category){
-	//do stuff
-	for(var i=0;i<results.length;i++){
-		if(results[i].category == category){
-			//add to div or whatever
-		}
-	}
-};
-
-
-//constraint is best before
+// order by best before
 function byBestBefore(results){
 	//sorting by best before date
 	results.sort(function(a,b){
@@ -72,7 +66,7 @@ function byBestBefore(results){
 	});
 };
 
-//constraint is alphabetical
+// order alphabetically
 function alphabetical(results){
 	//sorting by best before date
 	results.sort(function(a,b){
@@ -90,29 +84,35 @@ function alphabetical(results){
 
 $(document).ready(function(){
 	$("#sort").click(function(){
-		if(document.getElementById("A-Z").checked){
-			console.log("by alphabetical");
-			alphabetical(fooditems);
-			for (var i = fooditems.length - 1; i >= 0; i--) {
-				console.log(fooditems[i].foodname);
-			}
-		}else if(document.getElementById("nearest").checked){
-			console.log("by nearest")
-			byNearest(fooditems);
-			for (var i = fooditems.length - 1; i >= 0; i--) {
-				console.log(Math.sqrt(Math.pow(fooditems[i].marker.lat-54.78,2) + Math.pow(fooditems[i].marker.lng-(-1.52),2)));
-			}
-		}else if(document.getElementById("newest").checked){
-			console.log("by newest")
-		}else if (document.getElementById("freshest").checked){
-			console.log("freshest")
-			byBestBefore(fooditems)
-		}
+		var altered = false;
+		sortedResults = fooditems.slice();
 		if(document.getElementById("maxDistance").value != ""){
 			console.log("test max dis");
-			distanceRestriction(fooditems, document.getElementById("maxDistance").value);
+			sortedResults = distanceRestriction(sortedResults, document.getElementById("maxDistance").value);
+			altered = true;
 		}
-		writeCardHolder();
+		if(document.getElementById('bestBeforeDay').value != '' && document.getElementById('bestBeforeMonth').value != '' && document.getElementById('bestBeforeYear').value != ''){
+			sortedResults = bestBeforeRestriction(sortedResults, document.getElementById('bestBeforeDay').value, document.getElementById('bestBeforeMonth').value, document.getElementById('bestBeforeYear').value);
+			altered = true;
+		}
+		if(document.getElementById("A-Z").checked){
+			console.log("by alphabetical");
+			alphabetical(sortedResults);
+			altered = true;
+		}else if(document.getElementById("nearest").checked){
+			console.log("by nearest")
+			byNearest(sortedResults);
+			altered = true;
+		}else if(document.getElementById("newest").checked){
+			console.log("by newest")
+			altered = true;
+		}else if (document.getElementById("freshest").checked){
+			console.log("freshest")
+			byBestBefore(sortedResults)
+			altered = true;
+		}
+		if(true){
+			writeCardHolder(sortedResults);
+		}	
 	});
 });
-
