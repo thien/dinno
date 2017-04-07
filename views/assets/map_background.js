58f686d0c66e4338ff4ae331cdddabc3638c3ee7
JJ97 <johnmjennings97@gmail.com>
2017-04-07 14:42:20 +0100

    get location from cookies if possible

--- views/assets/map_background.js
+++ views/assets/map_background.js
@@ -1,8 +1,7 @@
 window.initMap = function() {
     // get latitude and longitude from GET variables
-    var coord_lat = 54.7731 ;
-    var coord_lng = -1.57489 ;
-
+    var coord_lat = parseFloat(Cookies.get('lat')) || 54.7731 ;
+    var coord_lng = parseFloat(Cookies.get('lng')) || -1.57489 ;
     // load google maps with 
     // console.log('hello');
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
