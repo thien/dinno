422981ebba719cc9d51f3f04f10762dc3e6f8ff4
SimeonC96 <simeonchan@live.com>
2017-04-15 17:28:40 +0100

    barcode search on live

--- views/assets/barcode.js
+++ views/assets/barcode.js
@@ -1,7 +1,7 @@
 
 function getMostFrequentBarcode(freq) {
     // Iterates through freq, keeping track of the barcode with the highest frequency
-    var mostFrequentBarcode = Object.keys(freq).reduce( function(a, b){ return freq[a] > freq[b] ? a : b } );
+    var mostFrequentBarcode = Object.keys(freq).reduce(function (a, b) { return freq[a] > freq[b] ? a : b });
     return mostFrequentBarcode;
 }
 
@@ -22,14 +22,15 @@ function run() {
             },
             debug: true,
         },
+        locate: true,
         decoder: { // or code_39, codabar, ean_13, ean_8, upc_a, upc_e
 
-          drawBoundingBox: true,
-          showFrequency: false,
-          drawScanline: true,
-          showPattern: false,
-            readers: ["code_128_reader",'ean_reader', 'ean_8_reader', 'code_39_reader', 'code_39_vin_reader', 'codabar_reader', 'upc_reader', 'upc_e_reader', 'i2of5_reader'],
-        }
+            drawBoundingBox: true,
+            showFrequency: false,
+            drawScanline: true,
+            showPattern: false,
+            readers: ["code_128_reader", 'ean_reader', 'ean_8_reader', 'code_39_reader', 'code_39_vin_reader', 'codabar_reader', 'upc_reader', 'upc_e_reader', 'i2of5_reader'],
+        },
     }, function (err) {
         if (err) {
             console.log(err);
@@ -37,21 +38,23 @@ function run() {
         console.log("Initialization finished. Ready to start");
         Quagga.start();
     });
-    Quagga.onDetected(function(data){
+    Quagga.onDetected(function (data) {
         console.log(decoded.length)
-        if(decoded.length > 50){
+        if (decoded.length > 50) {
             console.log(freq);
             var barcode = getMostFrequentBarcode(freq);
-            $('.food-name').val(barcode);
+            $.get("http://eandata.com/feed/", { v: 3, keycode: "C3AA45D258F231CD", mode: "json", find: barcode }, function (data) {
+                $("input#name").val(data.product.attributes.product)
+            })
             Quagga.stop();
-        }else{
-            decoded.splice(decoded.length,0,data.codeResult.code)
-            if(!freq[data.codeResult.code]){
+        } else {
+            decoded.splice(decoded.length, 0, data.codeResult.code)
+            if (!freq[data.codeResult.code]) {
                 freq[data.codeResult.code] = 1;
-            }else{
+            } else {
                 freq[data.codeResult.code] = freq[data.codeResult.code] + 1
             }
-            document.getElementsByClassName("drawingBuffer")[0].style["boxShadow"] = "inset 0px 0px 0px "+(decoded.length*2)+"px rgba(65,255,72,"+decoded.length/70+")";
+            document.getElementsByClassName("drawingBuffer")[0].style["boxShadow"] = "inset 0px 0px 0px " + (decoded.length * 2) + "px rgba(65,255,72," + decoded.length / 70 + ")";
         }
     })
 }
