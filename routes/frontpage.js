e776ffa5f2eb978829a35010e990133cbee1ee2a
Thien Nguyen <t.nguyen@outlook.com>
2017-04-20 22:07:11 +0100

    initial commit for server side socket working

--- routes/frontpage.js
+++ routes/frontpage.js
@@ -6,77 +6,73 @@ var db = require('../functions/database');
 var Cookies = require("cookies");
 
 function getBackgroundImage() {
-  return new Promise(function(resolve, reject) {
-    db.query(`SELECT Image FROM Meal
-              ORDER BY RAND()
-              LIMIT 20`,
-      function(error, results, fields) {
-        if (error) {
-          console.log(error);
-          reject(error);
-        } else {
-          var k = []
-          for (var i in results){
-            // console.log(i);
-            // console.log(results[i].image)
-            k.push(results[i].Image);
-          }
-          resolve(k);
-        }
-      });
-  });
+	return new Promise(function(resolve, reject) {
+		db.query(`SELECT Image FROM Meal
+			  ORDER BY RAND()
+			  LIMIT 20`,
+			function(error, results, fields) {
+				if (error) {
+					console.log(error);
+					reject(error);
+				} else {
+					var k = []
+					for (var i in results) {
+						// console.log(i);
+						// console.log(results[i].image)
+						k.push(results[i].Image);
+					}
+					resolve(k);
+				}
+			});
+	});
 }
 
-function getRandomUser(userID){
+function getRandomUser(userID) {
 	return new Promise(function(resolve, reject) {
 		db.query(`SELECT Firstname, Surname, UserID 
 							FROM User
 							WHERE User.UserID <> ?
 							ORDER BY RAND()
-							LIMIT 1`,[userID],
+							LIMIT 1`, [userID],
 			function(error, results, fields) {
 				if (error) {
 					console.log(error);
 					reject(error);
-				} 
-				else if (results.length == 0) {
+				} else if (results.length == 0) {
 					console.log(results);
 					console.log('No random user found');
 					reject('No random user found');
-				}
-				else{
+				} else {
 					var res = results[0];
 					var tip = {
-					msg: `Why not ask ${res.Firstname} ${res.Surname} if they want some of your spare food?`,
-					link: `/chat?id=${res.UserID}`
+						msg: `Why not ask ${res.Firstname} ${res.Surname} if they want some of your spare food?`,
+						link: `/chat?id=${res.UserID}`
+					}
+					resolve(tip);
 				}
-				resolve(tip);
-			}
-		});
+			});
 	});
 }
 
-function getRandomRecievedMeal(userID){
- return new Promise(function(resolve, reject) {
-   db.query(`SELECT Meal.Name, User.UserID, User.Firstname, User.Surname
+function getRandomRecievedMeal(userID) {
+	return new Promise(function(resolve, reject) {
+		db.query(`SELECT Meal.Name, User.UserID, User.Firstname, User.Surname
 						 FROM Meal
 						 JOIN User 
 						 ON Meal.UserID = User.UserID
 						 WHERE Meal.RecipientID = ?
 						 AND Meal.UserID <> ?
-             ORDER BY RAND()
-             LIMIT 1`,[userID, userID],
-     function(error, results, fields) {
-       if (error) {
+			 ORDER BY RAND()
+			 LIMIT 1`, [userID, userID],
+			function(error, results, fields) {
+				if (error) {
 					console.log(error);
 					reject(error);
-				} 
-				else if (results.length == 0) {
+				} else if (results.length == 0) {
 					console.log(results);
 					console.log('No random user found');
 					reject('No random user found');
-				}
-				else{
+				} else {
 					var res = results[0];
 					var tip = {
 						msg: `Why not ask ${res.Firstname} ${res.Surname} if they have any more ${res.Name}?`,
@@ -84,31 +80,29 @@ function getRandomRecievedMeal(userID){
 					}
 				}
 				resolve(tip);
-     });
- });
+			});
+	});
 }
 
-function getRandomDonatedMeal(userID){
- return new Promise(function(resolve, reject) {
-   db.query(`SELECT Meal.Name, User.UserID, User.Firstname, User.Surname
+function getRandomDonatedMeal(userID) {
+	return new Promise(function(resolve, reject) {
+		db.query(`SELECT Meal.Name, User.UserID, User.Firstname, User.Surname
 						 FROM Meal
 						 JOIN User 
 						 ON Meal.RecipientID = User.UserID
 						 WHERE Meal.UserID = ?
 					   AND Meal.RecipientID <> ?
-             ORDER BY RAND()
-             LIMIT 1`,[userID, userID],
-     function(error, results, fields) {
-       if (error) {
+			 ORDER BY RAND()
+			 LIMIT 1`, [userID, userID],
+			function(error, results, fields) {
+				if (error) {
 					console.log(error);
 					reject(error);
-				} 
-				else if (results.length == 0) {
+				} else if (results.length == 0) {
 					console.log(results);
 					console.log('No random user found');
 					reject('No random user found');
-				}
-				else{
+				} else {
 					var res = results[0];
 					var tip = {
 						msg: `Why not ask ${res.Firstname} ${res.Surname} if they want any more ${res.Name}?`,
@@ -116,31 +110,29 @@ function getRandomDonatedMeal(userID){
 					}
 				}
 				resolve(tip);
-     });
- });
+			});
+	});
 }
 
-function getRandomAvailableMeal(userID){
- return new Promise(function(resolve, reject) {
-   db.query(`SELECT Meal.Name, Meal.MealID, Location.Town
+function getRandomAvailableMeal(userID) {
+	return new Promise(function(resolve, reject) {
+		db.query(`SELECT Meal.Name, Meal.MealID, Location.Town
 						 FROM Meal
 						 JOIN Location
 						 ON Meal.LocationID = Location.LocationID
 						 WHERE Meal.UserID <> ?
 						 AND Meal.RecipientID IS NULL
-             ORDER BY RAND()
-             LIMIT 1`,[userID],
-     function(error, results, fields) {
-       if (error) {
+			 ORDER BY RAND()
+			 LIMIT 1`, [userID],
+			function(error, results, fields) {
+				if (error) {
 					console.log(error);
 					reject(error);
-				} 
-				else if (results.length == 0) {
+				} else if (results.length == 0) {
 					console.log(results);
 					console.log('No random user found');
 					reject('No random user found');
-				}
-				else{
+				} else {
 					var res = results[0];
 					var tip = {
 						msg: `${res.Name} in ${res.Town}?`,
@@ -148,50 +140,47 @@ function getRandomAvailableMeal(userID){
 					}
 				}
 				resolve(tip);
-     });
- });
+			});
+	});
 }
 
 function getHelpfulTip(userID) {
 	var choice = Math.random();
 	if (choice < 0.25) {
-	  return getRandomRecievedMeal(userID);
-	}
-	else if (choice < 0.5){
+		return getRandomRecievedMeal(userID);
+	} else if (choice < 0.5) {
 		return getRandomDonatedMeal(userID);
-	}
-	else if (choice < 0.75){
+	} else if (choice < 0.75) {
 		return getRandomAvailableMeal(userID);
-	}
-	else {
+	} else {
 		return getRandomUser(userID);
 	}
 }
 
 module.exports = function() {
-  app.locals.basedir = "." + '/views';
+	app.locals.basedir = "." + '/views';
 
-  app.get('/', function(req, res) {
-    var param = {
-      loggedin: false,
-    };
+	app.get('/', function(req, res) {
+		var param = {
+			loggedin: false,
+		};
 
-    getBackgroundImage().then(function(backgrounds) {
-      // param.background = background;
-      param.backgrounds = backgrounds;
+		getBackgroundImage().then(function(backgrounds) {
+			// param.background = background;
+			param.backgrounds = backgrounds;
 
-      login.checkLogin(req, res).then(function(result) {
-        param.loggedin = true;
-        userId = result.UserID;
-        param.user_data = {
-          userID: userId,
-          firstname: result.Firstname,
-          surname: result.Surname,
-          mugshot: result.ProfileImage,
-          textSize: result.TextSize,
-          colourScheme: result.ColourScheme,
-          isAdmin: result.IsAdmin
-        };
+			login.checkLogin(req, res).then(function(result) {
+				param.loggedin = true;
+				userId = result.UserID;
+				param.user_data = {
+					userID: userId,
+					firstname: result.Firstname,
+					surname: result.Surname,
+					mugshot: result.ProfileImage,
+					textSize: result.TextSize,
+					colourScheme: result.ColourScheme,
+					isAdmin: result.IsAdmin
+				};
 
 				getHelpfulTip(userId).then(function(tip) {
 					param.tip = tip;
@@ -206,14 +195,19 @@ module.exports = function() {
 					})
 				})
 
-      }, function(err) {
-        res.render('frontpage',param);
-      });
-    }, function(err) {
-      res.render('frontpage',param);
-    });
+			}, function(err) {
 
-  })
+				req.io.on('connection', function(socket) {
+					socket.emit('pdiddy', "haha");
+				});
 
-  return app;
+				res.render('frontpage', param);
+			});
+		}, function(err) {
+			res.render('frontpage', param);
+		});
+
+	})
+
+	return app;
 }();
