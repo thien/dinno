c97d7dc58391c1ca4049c0350db91e5adff101a0
SimeonC96 <simeonchan@live.com>
2017-04-21 11:23:28 +0100

    fixed search radius

--- routes/search.js
+++ routes/search.js
@@ -53,7 +53,7 @@ module.exports = function () {
 function iterateDistance(req, results, i, done) {
     if (i < results.length) {
         distance.get({
-            origin: "" + req.query.lat + "," + req.query.lng,
+            origin: "" + req.query.location,
             destination: "" + results[i].Latitude + "," + results[i].Longitude,
             units: 'metric'
         }, function (err, distanceData) {
